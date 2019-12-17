d05930c0da07beec8d6b523f92662b28769de3c9
zhangjiaxin0913 <e195739@ie.u-ryukyu.ac.jp>
2019-12-17 11:18:57 +0900

    report3

---
+++ src/jp/ac/uryukyu/ie/e195739/Main.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e195739;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Hero hero = new Hero("勇者", 10, 5);
+        Enemy enemy = new Enemy("スライム", 6, 3);
+
+        System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
+
+        int turn = 0;
+        while( hero.isDead() == false && enemy.isDead() == false ){
+            turn++;
+            System.out.printf("%dターン目開始！\n", turn);
+            hero.attack(enemy);
+            enemy.attack(hero);
+        }
+        System.out.println("戦闘終了");
+
+    }
+}
